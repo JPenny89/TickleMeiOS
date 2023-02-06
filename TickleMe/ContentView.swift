b5cf25343ecad8ce601f47476bd56970f7d7a6e0
James Penny <james_penny@hotmail.co.uk>
2023-02-06 20:41:15 +0000

    API call works but layout of response is currently all over the place. The limit is also entered via a Textfield. This needs to be hard-coded to "1" and the Textfield removed.

--- TickleMe/ContentView.swift
+++ TickleMe/ContentView.swift
@@ -8,14 +8,77 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    
+    @State var jokes = [Joke]()
+    @State var limit: String = ""
+    
+    init(){
+        UITableView.appearance().backgroundColor = .clear
+        UITableViewCell.appearance().backgroundColor = .clear
+    }
+    
+    func getJoke() {
+        Api().loadData(limit: self.limit) { (jokes) in
+            self.jokes = jokes
         }
-        .padding()
+    }
+    
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [Color.red, Color.purple]), startPoint: .top, endPoint: .bottom)
+            .edgesIgnoringSafeArea(.vertical)
+            .overlay(
+                VStack(alignment: .leading) {
+                    VStack {
+                        TextField(
+                            "Insert limit",
+                            text: $limit
+                        )
+                        .multilineTextAlignment(.center)
+                        .font(Font.title.weight(.light))
+                        .foregroundColor(Color.white)
+                        .padding()
+
+                        HStack {
+                            Spacer()
+                            Button(action: getJoke) {
+                                Text("Tickle Me")
+                                    .padding()
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 50)
+                                            .stroke(Color.white, lineWidth: 2)
+                                    )
+                            }
+                            .font(.title2)
+                            .foregroundColor(Color.white)
+                            Spacer()
+                        }
+                    }
+                    .padding(30.0)
+                    List {
+                        ForEach(jokes) { joke in
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Text("Joke 1 = \(joke.joke)")
+                                        .font(.title)
+                                        .padding(.bottom)
+                                                                        Text("Joke 2 = \(joke.id) ")
+                                                                            .font(.title2)
+                                }
+                                .minimumScaleFactor(0.01)
+                                Spacer()
+                                                                VStack(alignment: .trailing) {
+                                                                    Text("Joke 3 = \(joke.joke)")
+                                                                }
+                                    .minimumScaleFactor(0.01)
+                                    .font(.system(size: 18.0))
+                            }
+                            .listRowBackground(Color.clear)
+                            .foregroundColor(.black)
+                            .padding()
+                        }
+                    }
+                }
+            )
     }
 }
 
---
+++ TickleMe/Joke.swift
@@ -0,0 +1,43 @@
+//
+//  Joke.swift
+//  TickleMe
+//
+//  Created by James Penny on 06/02/2023.
+//
+
+import Foundation
+
+struct Joke: Codable, Identifiable {
+    let id = UUID()
+    var joke: String
+}
+
+class Api : ObservableObject{
+    @Published var jokes = [Joke]()
+
+    func loadData(limit: String, completion:@escaping ([Joke]) -> ()) {
+        let limit = limit.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        let url = URL(string: "https://api.api-ninjas.com/v1/dadjokes?limit="+limit!)!
+        var request = URLRequest(url: url)
+        request.setValue("0Rzx34mN+oPvjXnJFFuB3Q==AnmYCust1NLLgnAU", forHTTPHeaderField: "X-Api-Key")
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            let jokes = try! JSONDecoder().decode([Joke].self, from: data!)
+            print(jokes)
+            DispatchQueue.main.async {
+                completion(jokes)
+            }
+        }.resume()
+        
+//        let url = URL(string: "https://api.api-ninjas.com/v1/dadjokes?limit=1")!
+//        var request = URLRequest(url: url)
+//        request.setValue("0Rzx34mN+oPvjXnJFFuB3Q==AnmYCust1NLLgnAU", forHTTPHeaderField: "X-Api-Key")
+//        let task = URLSession.shared.dataTask(with: request) {(data, response, error) in
+//            guard let data = data else { return }
+//            print(String(data: data, encoding: .utf8)!)
+//            print("jokes = \()")
+//        }
+//        task.resume()
+        
+    }
+}
+
