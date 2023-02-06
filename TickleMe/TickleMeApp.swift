8727916f04bab683929912f91d331346e95adfc2
James Penny <james_penny@hotmail.co.uk>
2023-02-06 15:19:48 +0000

    Initial Commit

---
+++ TickleMe/TickleMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TickleMeApp.swift
+//  TickleMe
+//
+//  Created by James Penny on 06/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct TickleMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
